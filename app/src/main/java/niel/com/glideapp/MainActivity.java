071c089d539722ac22cba4a33a274bdcdd637f58
Nilesh Deokar <niel@Nileshs-MacBook-Air.local>
2017-07-26 21:56:19 +0530

    removed kotlin plugin and added extract image via glide to internal/external storage

--- app/src/main/java/niel/com/glideapp/MainActivity.java
+++ app/src/main/java/niel/com/glideapp/MainActivity.java
@@ -1,8 +1,15 @@
 package niel.com.glideapp;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,18 +19,32 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.signature.StringSignature;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
+import java.util.Random;
+
 import javax.microedition.khronos.opengles.GL;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getName().toString();
     private int count ;
     private long lastModified ;
     private EditText etUrl;
     private ImageView imageView;
     private TextView tvStatus;
+
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
         etUrl = (EditText) findViewById(R.id.editText);
 
         imageView.setTag(R.id.imageView,33);
+
+        etUrl.setText("https://cdn-images-1.medium.com/max/1200/1*hcfIq_37pabmAOnw3rhvGA.png");
     }
 
     private void loadImage(String url) {
@@ -67,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
                     Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
 
                     tvStatus.setText("Count :"+ count +" isFromCache : "+isFromMemoryCache);
+
                     return false;
                 }
             })
@@ -144,6 +168,132 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"Tag is : " + view.getTag(R.id.imageView),Toast.LENGTH_SHORT).show();
     }
 
+     /*
+      *  save image to external / internal storage using Glide
+      *
+      */
+     public void saveImage(View view){
+         String url = etUrl.getText().toString().trim();
+
+         Glide.with(this)
+                 .load(url)
+                 .asBitmap()
+                 .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                 .into(new SimpleTarget<Bitmap>() {
+                     @Override
+                     public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                         Log.d("Size ", "width :"+resource.getWidth() + " height :"+resource.getHeight());
+                         imageView.setImageBitmap(resource);
+                         storeImage(resource);
+                     }
+                 });
+
+     }
+
+    private void storeImage(Bitmap image) {
+        mBitmap = image;
+        File pictureFile = getOutputMediaFile();
+        try {
+        if (pictureFile == null) {
+
+            if(checkWriteExternalPermission()) {
+
+                FileOutputStream fos = getBaseContext().openFileOutput("file_name" + ".jps", Context.MODE_PRIVATE);
+                image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                fos.close();
+            }else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        1);
+            }
+            return;
+        }
+
+            if(checkWriteExternalPermission()) {
+
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                fos.close();
+            }else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        1);
+            }
+            Toast.makeText(this, "Image stored in : " + pictureFile, Toast.LENGTH_SHORT).show();
+
+            // image stored now set the url to the image path so we can test it using LOAD IMAGE Button
+            etUrl.setText(pictureFile.getPath());
+            Log.d(TAG, "img dir: " + pictureFile);
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+    }
+
+
+    private  File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        if(isSdPresent()) {
+            File   mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                    + "/Android/data/"
+                    + getApplicationContext().getPackageName()
+                    + "/Files");
+
+
+            if (!mediaStorageDir.exists()) {
+                if (!mediaStorageDir.mkdirs()) {
+                    return null;
+                }
+            }
+
+            File mediaFile;
+            Random generator = new Random();
+            int n = 1000;
+            n = generator.nextInt(n);
+            String mImageName = "Image-" + n + ".jpg";
+
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+            return mediaFile;
+        }else {
+            Toast.makeText(this, "SD Card not present", Toast.LENGTH_SHORT).show();
+        }
+        return null;
+
+    }
+
+
+    private boolean checkWriteExternalPermission()
+    {
+        int res = this.checkCallingOrSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return (res == PackageManager.PERMISSION_GRANTED);
+    }
+
+    public static boolean isSdPresent() {
+
+        return android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    storeImage(mBitmap);
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission denied to write your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
 
 
    /* User RequestManager Object in Recyclerview
